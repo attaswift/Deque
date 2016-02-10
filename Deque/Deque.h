9aa6474b5b5ed0e7cd5e2c27e01a3d72622cef95
Karoly Lorentey <karoly@lorentey.hu>
2016-02-10 17:37:38 +0100

    Initial Commit

---
+++ Deque/Deque.h
@@ -0,0 +1,19 @@
+//
+//  Deque.h
+//  Deque
+//
+//  Created by Károly Lőrentey on 2016-02-10.
+//  Copyright © 2016 Károly Lőrentey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Deque.
+FOUNDATION_EXPORT double DequeVersionNumber;
+
+//! Project version string for Deque.
+FOUNDATION_EXPORT const unsigned char DequeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Deque/PublicHeader.h>
+
+
